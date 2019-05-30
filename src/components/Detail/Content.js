de39a8bd9511c06d2b515285cd6e4c0cd9f8243e
Timm <timmcliu@gmail.com>
2019-05-30 02:05:53 -0700

    detail screen images

--- src/components/Detail/Content.js
+++ src/components/Detail/Content.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import styles from './Content.module.css'
 import Breadcrumbs from '../Breadcrumbs'
-import Hero from '../Hero'
 
 const Content = (brawler) => {
+  const image = require(`../../assets/images/${brawler.avatar}`)
+
   return (
     <div className={styles.content}>
       <Breadcrumbs name={ brawler.name } />
-      <Hero />
-      {/* images */}
-      <p>{ brawler.description }</p>
+
+      <div className={styles.heroWrapper}>
+        <img src={image} alt={brawler.name} className={styles.hero} />
+      </div>
+
+      <div className={styles.thumbs}>
+        <img src={image} alt={brawler.name} className={styles.thumb} />
+        <img src={image} alt={brawler.name} className={styles.thumb} />
+        <img src={image} alt={brawler.name} className={styles.thumb} />
+        <img src={image} alt={brawler.name} className={styles.thumb} />
+        <img src={image} alt={brawler.name} className={styles.thumb} />
+      </div>
+
+      <p className={styles.descriptionWrapper}><span className={styles.description}>{ brawler.description }</span></p>
     </div>
   )
 }
