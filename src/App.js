e193333c3841e452497b00f00104de30aba5b478
Timm <timmcliu@gmail.com>
2019-05-29 21:36:29 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router } from '@reach/router';
+import Home from './screens/Home';
+import Detail from './screens/Detail';
+
+let HomeRoute = () => <Home />
+let DetailRoute = () => <Detail />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <HomeRoute path="/" />
+        <DetailRoute path="detail" />
+      </Router>
     </div>
   );
 }
