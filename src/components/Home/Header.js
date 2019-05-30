c70916336c90476a09c943adc7cc73b8eac72aa5
Timm <timmcliu@gmail.com>
2019-05-30 11:30:42 -0700

    text, title

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -4,8 +4,8 @@ import styles from './Header.module.css'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Brawl Stars</h1>
-      <h3>A gallery of favorite brawlers</h3>
+      <h1 className={styles.title}>React Brawlers</h1>
+      <h3>A gallery of favorite brawlers from Supercell's Brawl Stars</h3>
     </header>
   );
 }
