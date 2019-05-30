4fb0f4a8af447f18212ad5b7d35824f5cbbd0f64
Timm <timmcliu@gmail.com>
2019-05-29 23:23:22 -0700

    more brawlers, carousel setup

--- src/components/Home/Gallery.js
+++ src/components/Home/Gallery.js
@@ -9,9 +9,11 @@ const Gallery = () => {
   return (
     <div className={styles.gallery}>
       <ChevronLeft size={64} className={styles.chevron} />
-      { data.brawlers.map(brawler => {
-        return <Card brawler={brawler} key={brawler.name} />
-      }) }
+      <div className={styles.carousel}>
+        { data.brawlers.map(brawler => {
+          return <Card brawler={brawler} key={brawler.name} />
+        }) }
+      </div>
       <ChevronRight size={64} className={styles.chevron} />
       <Dots brawlers={data.brawlers} />
     </div>
--- src/data.js
+++ src/data.js
@@ -12,6 +12,30 @@ const brawlers = [
     "description": ""
   },
   {
+    "name": "Carl",
+    "quote": "Carl throws his Pickaxe like a boomerang. His Super is a crazy cart spin that clobbers anyone around him.",
+    "avatar": "carl.png",
+    "description": ""
+  },
+  {
+    "name": "Poco",
+    "quote": "Poco fires damaging sound waves at enemies. His Super can heal both Poco himself and his teammates!",
+    "avatar": "poco.png",
+    "description": ""
+  },
+  {
+    "name": "Penny",
+    "quote": "Penny shoots bags of coins, damaging the target and anyone standing behind. Her Super is a mortar-style cannon turret!",
+    "avatar": "penny.png",
+    "description": ""
+  },
+  {
+    "name": "Gene",
+    "quote": "Gene uses his magic lamp to shoot a splitting projectile. His super is a magical hand that grabs and pulls enemies close!",
+    "avatar": "gene.png",
+    "description": ""
+  },
+  {
     "name": "Spike",
     "quote": "Spike throws cactus grenades that send needles flying, and a show-stopping Super: a field of cactus spines that slows down and damages enemies!",
     "avatar": "spike.png",
